050a71aec5a2d05720db1cf0f817086474743a48
손다인 <dain0826@naver.com>
2024-07-02 17:54:05 +0900

    ProductResponse ãì추가

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -3,9 +3,18 @@ package sample.cafekiosk.spring.api.service.product;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
+import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+
+    public List<ProductResponse> getSellingProducts(){
+
+    }
 }
